8473b0c9c41511936531b93bafc13a9ff1000711
bilol-anvarov <bilolanvarov8080@gmail.com>
2025-03-21 20:34:59 +0500

    upd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import "./Components.css";
 import { useRouter } from "next/navigation";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Header() {
   const router = useRouter();
@@ -21,44 +22,82 @@ export default function Header() {
   }, [router.events]);
 
   return (
-    <header className="header fixed  w-full flex justify-center">
-      <div className="header_ins defCtr">
-        <Link href={"/"}>
-          <div className="logoCtr">
-            <img src="/logo.svg" alt="logo" />
-            <img src="/logoName.svg" alt="logo" />
-          </div>
-        </Link>
-        <nav>
-          <ul className="links desktop flex lg:gap-[74px] gap-[30px]">
-            <li>
-              <Link href="/">Паллеты</Link>
-            </li>
-            <li>
-              <Link href="/">Деревянные ящики</Link>
-            </li>
-            <li>
-              <Link href="/">Производство</Link>
-            </li>
-            <li>
-              <Link href="/">О нас</Link>
-            </li>
-            <li>
-              <Link href="/">Контакты</Link>
-            </li>
-          </ul>
-        </nav>
-        <nav className={`mobile${ifOpenBar ? " active" : ""}`}>
-          <div className="navInside"></div>
-        </nav>
-        <div className="burgerMobile" onClick={()=>{setIfOpenBar(item=> item = !ifOpenBar)}}>
-          <div className={`iconMenu humburger${ifOpenBar ? " active" : ""}`}>
-            <div className="bar bar--1"></div>
-            <div className="bar bar--2"></div>
-            <div className="bar bar--3"></div>
+    <>
+      <header className="header fixed  w-full flex justify-center">
+        <div className="header_ins defCtr">
+          <Link href={"/"}>
+            <div className="logoCtr">
+              <img src="/logo.svg" alt="logo" />
+              <img src="/logoName.svg" alt="logo" />
+            </div>
+          </Link>
+          <nav>
+            <ul className="links desktop flex lg:gap-[74px] gap-[30px]">
+              <li>
+                <Link href="/">Паллеты</Link>
+              </li>
+              <li>
+                <Link href="/">Деревянные ящики</Link>
+              </li>
+              <li>
+                <Link href="/">Производство</Link>
+              </li>
+              <li>
+                <Link href="/">О нас</Link>
+              </li>
+              <li>
+                <Link href="/">Контакты</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div
+            className="burgerMobile"
+            onClick={() => {
+              setIfOpenBar((item) => (item = !ifOpenBar));
+            }}
+          >
+            <div className={`iconMenu humburger${ifOpenBar ? " active" : ""}`}>
+              <div className="bar bar--1"></div>
+              <div className="bar bar--2"></div>
+              <div className="bar bar--3"></div>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <AnimatePresence>
+        {ifOpenBar && (
+          <motion.div
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 1 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className={`navMobileCtr  w-full  fixed left-[0] top-[0] h-full ${ifOpenBar && 'active'} `}
+          >
+            <nav className={`mobile`}>
+              <div className="navInside w-full">
+                <ul className="links desktop flex lg:gap-[74px] gap-[30px]">
+                  <li>
+                    <Link href="/">Паллеты</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Деревянные ящики</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Производство</Link>
+                  </li>
+                  <li>
+                    <Link href="/">О нас</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Контакты</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
