e42f98eade217025ba16de9253aed2ee364ef25d
bilol-anvarov <bilolanvarov8080@gmail.com>
2025-03-25 01:56:19 +0500

    upd

--- src/app/boxes/page.jsx
+++ src/app/boxes/page.jsx
@@ -1,12 +1,13 @@
 import Banner from '@/components/Banner'
+import Boxes from '@/components/Boxes'
 import React from 'react'
 
 export default function Page() {
   return (
-    <div className='boxesPage'>
+    <div className='boxesPage defCtr'>
         <Banner imgSrc={'/image 43.png'} title={'Деревянные ящики'}/>
         <h2 className='text-center mt-[60px]'>Виды производимых ящиков</h2>
-
+        <Boxes />
     </div>
   )
 }
