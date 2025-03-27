2f1051cc8cc5940acd1f6918c165061ad08c9f40
IMOWWW <imowww@yandex.ru>
2025-03-27 16:10:07 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
       name: "ecopallety",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next", 
       args: "start -p 3030",
       env: {
         NODE_ENV: "production"
